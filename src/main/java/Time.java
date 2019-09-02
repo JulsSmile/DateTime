16fdb750bd7ac84554eb576316e359a481f38176
Yuliia Horenko <yuliia_horenko@epam.com>
2019-09-02 17:15:03 +0300

    date time commit

--- src/main/java/Time.java
+++ src/main/java/Time.java
@@ -3,6 +3,7 @@ import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 
 
+import org.joda.time.LocalDateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
@@ -14,7 +15,7 @@ public class Time{
         System.out.println(currentDate);
 
 
-       String data = String.valueOf(currentDate);
+        String data = String.valueOf(currentDate);
         DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd");
         DateTime jodatime = dtf.parseDateTime(data);
         DateTimeFormatter dtfOut = DateTimeFormat.forPattern("MM/dd/yyyy");
@@ -22,8 +23,11 @@ public class Time{
         DateTimeFormatter dateformatOut = DateTimeFormat.forPattern("dd-MM-yyyy");
         System.out.println(dateformatOut.print(jodatime));
 
+        DateTimeFormatter dateTime = DateTimeFormat.longDateTime();
+        System.out.println(dateTime.print(jodatime));
+        }
     }
-}
+
 
 
 
