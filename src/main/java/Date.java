c3c1b35c496438d3e14a506a5ec4683b14a215ef
Yuliia Horenko <yuliia_horenko@epam.com>
2019-09-02 16:39:30 +0300

    date time commit

---
+++ src/main/java/Date.java
@@ -0,0 +1,10 @@
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+
+public class Date{
+    public static void main(String[] args) {
+
+        }
+    }
+
---
+++ src/main/java/Time.java
@@ -0,0 +1,29 @@
+import org.joda.time.DateTime;
+
+import org.joda.time.LocalDate;
+
+
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+
+public class Time{
+    public static void main(String[] args) {
+
+        LocalDate currentDate = LocalDate.now();
+        System.out.println(currentDate);
+
+
+       String data = String.valueOf(currentDate);
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd");
+        DateTime jodatime = dtf.parseDateTime(data);
+        DateTimeFormatter dtfOut = DateTimeFormat.forPattern("MM/dd/yyyy");
+        System.out.println(dtfOut.print(jodatime));
+        DateTimeFormatter dateformatOut = DateTimeFormat.forPattern("dd-MM-yyyy");
+        System.out.println(dateformatOut.print(jodatime));
+
+    }
+}
+
+
+
