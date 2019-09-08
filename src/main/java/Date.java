636b5d5fba2254255ad6a0fd00c4a2988e712f10
julia <julshrenka@gmail.com>
2019-09-08 21:18:56 +0300

    start and end of the day

--- src/main/java/Date.java
+++ src/main/java/Date.java
@@ -1,7 +1,3 @@
-import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
-import org.joda.time.LocalDateTime;
-
 public class Date{
     public static void main(String[] args) {
 
--- src/main/java/Time.java
+++ src/main/java/Time.java
@@ -1,40 +1,24 @@
-import org.joda.time.DateTime;
 
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDate;
-
-
-import org.joda.time.LocalDateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 
 
 public class Time{
     public static void main(String[] args) {
 
-        LocalDate currentDate = LocalDate.now();
-        System.out.println(currentDate);
-        LocalDateTime currentTime = LocalDateTime.now();
+//        LocalDate currentDate = LocalDate.now();
+//        System.out.println(currentDate);
+//        LocalDateTime currentTime = LocalDateTime.now();
 
 
-        String data = String.valueOf(currentDate);
-        DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd");
-        DateTime jodatime = dtf.parseDateTime(data);
-        DateTimeFormatter dtfOut = DateTimeFormat.forPattern("MM/dd/yyyy");
-        System.out.println(dtfOut.print(jodatime));
-        DateTimeFormatter dateformatOut = DateTimeFormat.forPattern("dd-MM-yyyy");
-        System.out.println(dateformatOut.print(jodatime));
+//        String data = String.valueOf(currentDate);
+//        DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd");
+//        DateTime jodatime = dtf.parseDateTime(data);
+//        DateTimeFormatter dtfOut = DateTimeFormat.forPattern("MM/dd/yyyy");
+//        System.out.println(dtfOut.print(jodatime));
+//        DateTimeFormatter dateformatOut = DateTimeFormat.forPattern("dd-MM-yyyy");
+//        System.out.println(dateformatOut.print(jodatime));
 
-//        LocalDateTime ldt = LocalDateTime.of(2014, 5, 29, 18, 41, 16);
-//        ZonedDateTime zdt = ldt.atZone(ZoneId.of("America/Los_Angeles"));
-//        System.out.println("ldt " + LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
-//        System.out.println("ctm " + System.currentTimeMillis());
-//        public DateTime toDateTimeAtStartOfDay(DateTimeZone);
-//        System.out.println(dateStartDay.print(jodatime));
-//        LocalDate.parse(stringDate, DATEFORMATTER).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli();
+//        jodatime.with
+
 
 
     }
